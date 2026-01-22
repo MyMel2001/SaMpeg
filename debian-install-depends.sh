063027ab9076af7acd8025ca46fb483840c774e5
MyMel2001 Holder <163201376+mymel2001-holder@users.noreply.github.com>
2026-01-22 03:53:04 +0000

    Update debian-install-depends.sh

--- debian-install-depends.sh
+++ debian-install-depends.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-# Update package lists
-sudo apt update
-
-# Install dependencies
-sudo apt install -y ffmpeg toilet bc
-
+sudo apt update && sudo apt install -y ffmpeg zenity bc x11-utils libvidstab-dev
